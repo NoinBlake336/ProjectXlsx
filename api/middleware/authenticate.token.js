cbe1ee74fea1d090005c1de8dd57df9237cebf59
NoinBlake336 <144173984+NoinBlake336@users.noreply.github.com>
2023-11-21 16:46:37 +0000

    validate

--- api/middleware/authenticate.token.js
+++ api/middleware/authenticate.token.js
@@ -1,2 +1,22 @@
 const jwt = require('jsonwebtoken');
-const 
+const {config}  = require('../cofing');
+const boom = require('@hapi/boom');
+
+const authenticateToken = (req,res,next)=>{
+    const token = req.headers['token'];
+    if(token){
+        jwt.verify(token,config.secretKey,(err,decoded)=>{
+            if(err){
+                throw boom.illegal('El token es invalido');
+            }
+
+            req.user = decoded;
+            next();
+        })
+    }else{
+        res.status(401);
+    }
+}
+
+
+module.exports = authenticateToken;
