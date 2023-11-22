da8cb3c4d8f86656537eaa90368b3bee7c06093b
NoinBlake336 <144173984+NoinBlake336@users.noreply.github.com>
2023-11-22 15:53:39 +0000

    products

--- api/router/index.js
+++ api/router/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const userRouter = require('../components/users');
 const authRouter = require('../components/auth');
+const productRouter = require('./components/products');
 const Router = (app)=>{
     const router = express.Router();
     app.use('/api', router);
     router.use('/users',userRouter);
     router.use('/auth',authRouter);
+    router.use('/products',productRouter);
 };
 
 module.exports = Router;
