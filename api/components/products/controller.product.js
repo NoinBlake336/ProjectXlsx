bcc6cba3bff690dc6919d5f52816ce91299b20b4
NoinBlake336 <144173984+NoinBlake336@users.noreply.github.com>
2023-11-22 03:54:48 +0000

    products

--- api/components/products/controller.product.js
+++ api/components/products/controller.product.js
@@ -1,9 +1,35 @@
 const ProductServices = require('./service');
-const controller = new ProductServices();
+const service = new ProductServices();
 
 
 class ControllerProducts {
-    
+    async find(query){
+        const totalPages = await service.getTotalPage(7);
+        const options = {};
+        const {
+            page,
+            limit
+        } = query;
+
+        if(page && limit) {
+            options.limit = parseInt(limit);
+            options.skip = (parseInt(page)-1) * parseInt(limit);
+        };
+
+        const getProducts = await service.getProducts(options);
+        const result = {
+            produt:getProducts,
+            page:totalPages,
+        };
+        return result;
+    };    
+
+    async findOne(id){
+        const product = await service.getOneProducts(id);
+        return {product}
+    }
+
+
 };
 
 
--- api/components/products/service/index.js
+++ api/components/products/service/index.js
@@ -2,7 +2,29 @@ const boom = require('@hapi/boom');
 const Model = require('../model');
 
 class ProductServices {
+    async getTotalPage(elementsPerPage){
+        const totalElements = await Model.countDocuments();
+        let totalPages = Math.floor(totalElements / elementsPerPage);
 
+        if(totalElements % elementsPerPage !== 0){
+            totalPages++;
+        };
+        return totalPages;
+    };
+
+    async getProducts(options){
+        const products = await Model.findAll({},null,options);
+        return products;
+    }
+
+    async getOneProducts(id){
+        const product = await Model.findById(id);
+        if(!product){
+            boom.badRequest('Not Product');
+        };
+
+        return product;
+    }
 };
 
 
