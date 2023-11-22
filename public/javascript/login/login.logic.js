c1412ac1e0a36bbbd6b295e096ef272d5f9e849b
NoinBlake336 <144173984+NoinBlake336@users.noreply.github.com>
2023-11-22 15:45:25 +0000

    auth

--- public/javascript/login/login.logic.js
+++ public/javascript/login/login.logic.js
@@ -29,7 +29,7 @@ window.addEventListener("click",(e)=>{
 });
 
 const validatorLogin = (email,password)=>{
-    return fetch('https://project-xlsx-backend.vercel.app/api/login',{
+    return fetch('https://project-xlsx-backend.vercel.app/api/auth',{
         method:'POST',
         headers:{
             'Content-Type':'application/json',
