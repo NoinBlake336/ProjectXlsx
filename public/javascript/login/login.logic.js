21b0eadf026a565dc2248674069f8c91ad0cbc3d
NoinBlake336 <144173984+NoinBlake336@users.noreply.github.com>
2023-11-25 20:35:59 +0000

    login

--- public/javascript/login/login.logic.js
+++ public/javascript/login/login.logic.js
@@ -3,30 +3,19 @@ const inputEmail = document.getElementById('email');
 const inputPass = document.getElementById('pass');
 const messageError = document.querySelectorAll('#message-error');
 
-const MessageError = (err)=>{
-        messageError[0].innerHTML=err;
-        messageError[1].innerHTML=err;
-        messageError[0].classList.remove('hidden');
-        messageError[1].classList.remove('hidden');
-        inputEmail.style.border = 'solid red 2px';
-        inputPass.style.border = 'solid red 2px';
+const setUserinLocalStorage = (user)=>{
+    localStorage.setItem('user',JSON.stringify(user));
 }
-window.addEventListener("click",(e)=>{
-    const button = e.target.id;
-    if(button !== 'frame_2'){
-        return;
-    };
-    const login = body.activeElement.parentNode.parentNode.parentNode.childNodes[1];
-    const email = login.children[0].children[1];
-    const password = login.children[1].children[1];
 
-    if(email.value === "" && password.value ===""){
-        return MessageError('Completa el campo');
-    }
+const MessageError = (err)=>{
+    messageError[0].innerHTML=err;
+    messageError[1].innerHTML=err;
+    messageError[0].classList.remove('hidden');
+    messageError[1].classList.remove('hidden');
+    inputEmail.style.border = 'solid red 2px';
+    inputPass.style.border = 'solid red 2px';
+}
 
-    validatorLogin(email,password); 
-
-});
 
 const validatorLogin = (email,password)=>{
     return fetch('https://project-xlsx-backend.vercel.app/api/auth',{
@@ -51,8 +40,7 @@ const validatorLogin = (email,password)=>{
         const user = data.user;
         if(token){
 
-            setTokenInCookie(token);
-            setUserinLocalStorage(data); 
+            setUserinLocalStorage({user:data.user,token:data.token}); 
             messageError[1].innerHTML='Acccount valide';
             messageError[0].classList.add('hidden');
             messageError[1].classList.remove('hidden');
@@ -71,32 +59,32 @@ const validatorLogin = (email,password)=>{
 };
 
 
-const setTokenInCookie = (token)=>{
-    const expirationDate = new Date(); 
-    expirationDate.setMinutes(expirationDate.getMinutes()+15);
-    document.cookie = `token=${token}; expires=${expirationDate.toUTCString()}; `;
-    return getTokenFromCookie();
-};
+window.addEventListener("click",(e)=>{
+    const button = e.target.id;
+    if(button !== 'frame_2'){
+        return;
+    };
+    const login = body.activeElement.parentNode.parentNode.parentNode.childNodes[1];
+    const email = login.children[0].children[1];
+    const password = login.children[1].children[1];
 
-const setUserinLocalStorage = (user)=>{
-    localStorage.setItem('user',JSON.stringify(user));
-}
-
-const getTokenFromCookie = ()=>{
-    const cookies = document.cookie.split(';');
-    for(let i = 0; i < cookies.length; i++){
-        const cookie = cookies[i].trim();
-        if(cookie.startsWith('token=')){
-            return cookie.substring('token='.length);
-        }
+    if(email.value === "" && password.value ===""){
+        return MessageError('Completa el campo');
     }
-}
+
+    validatorLogin(email,password); 
+
+});
+
+
+
+
+
+
+
+
 
 const redirectToDashBoard = async()=>{
-    const token = getTokenFromCookie();
-    if(token){
-        
-        window.location.href = ('/dashboard');
-    }
+    window.location.href = ('/dashboard');
 }
 
